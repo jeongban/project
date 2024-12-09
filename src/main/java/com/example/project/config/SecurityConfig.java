1a42cac1d9dc2dac860aa21f86d049568aeba53e
jeong <cox82910@naver.com>
2024-12-09 19:05:33 +0900

    feat: spring security 설정

---
+++ src/main/java/com/example/project/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .anyRequest().permitAll()); // 페이지에 권한이 없음
+        return http.build();
+    }
+}
